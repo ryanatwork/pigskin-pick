eef5f974749c327649c1195cd37fea92cef4ae4c
Ryan Resella <ryanresella@gmail.com>
2013-07-20 13:22:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pigskinpick::Application.config.secret_key_base = '7fd521b32e3002a25c1f29a1d991bb1238c25726488a4249a64e167257cf4f7c7d7cb23787e9319694697f3cb737963c8b2d95b68c439caa8b2216f1ae075f89'
